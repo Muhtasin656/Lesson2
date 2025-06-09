13a7a32976c4743266b26f1f21f68fd6808f3bdc
Muhtasin656 <muhtasinmasrur656@gmail.com>
2025-06-09 12:42:57 +1000

    New

---
+++ Acp2.py
@@ -0,0 +1,8 @@
+birth=input("enter your birth")
+print("good luck on",birth)
+birth=input("enter your birth")
+print("good luck on",birth)
+birth=input("enter your birth")
+print("good luck on",birth)
+birth=input("enter your birth")
+print("good luck on",birth)
